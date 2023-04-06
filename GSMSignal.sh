08ec8bad321acb2f7473c2d954db39f0204bc799
GSM ADJAA <96638829+jacquesTech@users.noreply.github.com>
2023-04-06 18:12:41 +0200

    Update GSMSignal.sh

--- GSMSignal.sh
+++ GSMSignal.sh
@@ -47,7 +47,7 @@ $folderBin/ideviceactivation activate -d -s PHP NOT FREE IF YOU NEED BY CONTACT 
 $folderBin/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p2222 '/usr/libexec/substrate';
 $folderBin/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p2222 '/usr/libexec/substrated';
 $folderBin/idevicepair pair;
-$folderBin/ideviceactivation activate -d -s https://bigb033xecution3r.com/iOS14/Activation/ActivateDevice.php;
+$folderBin/ideviceactivation activate -d -s PHP NOT FREE IF YOU NEED BY CONTACT ME ON THE LINK GIVEN;
 $folderBin/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p2222 'rm /Library/MobileSubstrate/DynamicLibraries/untethered.dylib';
 $folderBin/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p2222 'rm /Library/MobileSubstrate/DynamicLibraries/untethered.plist';
 $folderBin/sshpass -p 'alpine' scp -P 2222 -p "/Users/$(whoami)/Desktop/tmp/Cache/5g" root@localhost:"/./Library/MobileSubstrate/DynamicLibraries/iuntethered.dylib";
